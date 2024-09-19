ed38caf4299a43895e4e3b088ac4d0a1c81ec365
itscoooldeep <65156813+itscoooldeep@users.noreply.github.com>
2024-09-19 19:01:15 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,76 @@
+
+a=2
+b=3
+
+print( a+b)
+print( a-b)
+product= a*b
+qutiont = a/b
+print("product" ,product)
+print(qutiont,"quitiont")
+
+c=a>b
+print(c,"a is not greater than b")
+
+d=a<b
+print(d,"a is less than b")
+
+e= a<= b
+print(e,"less than equal to")
+
+f= a>=b
+print(f,"greater than equal to")
+
+g= a==b
+print(g, "a is equal to b ")
+
+h= a!=b
+print(h,"a is not equal to b")
+
+print("-----####$$$$$%%%%%%-------")
+
+x = True
+y= False
+
+and_result= x and y
+or_result= x or y
+not_result_x= not x
+not_result_y= not y
+
+print("x and y",and_result)
+print(" x or y",or_result)
+print(" not x",not_result_x)
+print("not y",not_result_y)
+
+
+print("-----####$$$$$%%%%%%-------")
+
+
+total = 10
+
+total += 5
+total -= 3
+total *= 2
+total /= 4
+
+print("Final total:", total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
