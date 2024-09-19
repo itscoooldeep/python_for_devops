c4096be3e8e3d963b60cb7235007cb3a8054f267
itscoooldeep <65156813+itscoooldeep@users.noreply.github.com>
2024-09-19 22:56:13 +0530

    Create slicing.py
    day3

---
+++ slicing.py
@@ -0,0 +1,9 @@
+##slicing
+str= "kuldeep is here"
+print(str[1:4])
+print(str[:4])  #it will print 0 index
+print(str[2:])  #it will print till last index
+
+##negative indexing
+str1 = "apple"
+print(str1[-3:-1])
